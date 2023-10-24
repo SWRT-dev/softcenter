b78a51d1e085e5580ed0ce99b56180a8e1c4127b
ZhaoWei Xu <paldier@hotmail.com>
2023-10-24 15:48:00 +0800

    update ddns

--- ddns/config.json.js
+++ ddns/config.json.js
@@ -1,9 +1,9 @@
 {
-"version":"0.1",
-"md5":"bb349d0e7bb3467b0ec19030482438e7",
+"version":"0.2",
+"md5":"dff948de2166f1796de89b1867c1f03b",
 "arch":"mips",
 "home_url":"Module_ddns.asp",
 "title":"多ddns合一",
 "description":"支持Alidns(阿里云) Dnspod(腾讯云) Cloudflare 华为云 Callback",
-"build_date":"2022-03-20_15:55:10"
+"build_date":"2023-10-24_15:47:51"
 }
