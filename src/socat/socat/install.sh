3f6decd4b9bcb36c33124e411e159c299e9fa618
superzjg <115226261+superzjg@users.noreply.github.com>
2025-07-26 18:03:00 +0800

    Add files via upload

--- src/socat/socat/install.sh
+++ src/socat/socat/install.sh
@@ -2,38 +2,30 @@
 source /jffs/softcenter/scripts/base.sh
 eval `dbus export socat_`
 alias echo_date='echo 【$(TZ=UTC-8 date -R +%Y年%m月%d日\ %X)】:'
-MODEL=$(nvram get productid)
 DIR=$(cd $(dirname $0); pwd)
-if [ "${MODEL:0:3}" == "GT-" ] || [ "$(nvram get swrt_rog)" == "1" ];then
-	ROG=1
-elif [ "${MODEL:0:3}" == "TUF" ] || [ "$(nvram get swrt_tuf)" == "1" ];then
-	TUF=1
-fi
-if [ "$socat_enable"x = "1"x ] ; then
-    sh /jffs/softcenter/scripts/socat_config.sh stop
-fi
-# 安装插件
+
+# 皮肤相关操作交由软件中心主安装脚本
+
+#旧版文件
+[ -f "/jffs/softcenter/scripts/socat_start.sh" ] && {
+	sh /jffs/softcenter/scripts/socat_start.sh stop
+	rm -f /jffs/softcenter/scripts/socat_start.sh
+	echo_date "已删除旧版文件"
+}
+# 安装
+sh /jffs/softcenter/scripts/socat_config.sh stop >/dev/null 2>&1
+echo_date "插件安装中..."
 cd $DIR
 find /jffs/softcenter/init.d/ -name "*socat*"|xargs rm -rf
 cp -rf $DIR/scripts/* /jffs/softcenter/scripts/
 cp -rf $DIR/webs/* /jffs/softcenter/webs/
 cp -rf $DIR/res/* /jffs/softcenter/res/
 cp -f $DIR/uninstall.sh /jffs/softcenter/scripts/uninstall_socat.sh
-ln -sf /jffs/softcenter/scripts/socat_start.sh /jffs/softcenter/init.d/S99socat.sh
-if [ "$ROG" == "1" ];then
-	continue
-elif [ "$TUF" == "1" ];then
-	sed -i 's/3e030d/3e2902/g;s/91071f/92650F/g;s/680516/D0982C/g;s/cf0a2c/c58813/g;s/700618/74500b/g;s/530412/92650F/g' /jffs/softcenter/webs/Module_socat.asp >/dev/null 2>&1
-else
-	sed -i '/rogcss/d' /jffs/softcenter/webs/Module_socat.asp >/dev/null 2>&1
-fi
-
-chmod 755 /jffs/softcenter/scripts/socat_config.sh
-chmod 755 /jffs/softcenter/scripts/socat_start.sh
-chmod 755 /jffs/softcenter/scripts/socat_status.sh
-chmod 755 /jffs/softcenter/scripts/uninstall_socat.sh
+ln -sf /jffs/softcenter/scripts/socat_config.sh /jffs/softcenter/init.d/N99socat.sh
+chmod 0755 /jffs/softcenter/scripts/*socat*
 
 # 离线安装用
+echo_date "设置默认值及软件中心注册信息..."
 dbus set socat_version="$(cat $DIR/version)"
 dbus set softcenter_module_socat_version="$(cat $DIR/version)"
 dbus set softcenter_module_socat_description="Socat 端口转发"
@@ -43,5 +35,5 @@ dbus set softcenter_module_socat_title="Socat端口转发"
 
 # 完成
 echo_date "Socat端口转发插件安装完毕！"
-rm -rf $DIR/* >/dev/null 2>&1
+rm -rf $DIR >/dev/null 2>&1
 exit 0
--- src/socat/socat/uninstall.sh
+++ src/socat/socat/uninstall.sh
@@ -1,24 +1,29 @@
 #!/bin/sh
 
-eval `dbus export socat_`
 source /jffs/softcenter/scripts/base.sh
 alias echo_date='echo 【$(TZ=UTC-8 date -R +%Y年%m月%d日\ %X)】:'
 
-/jffs/softcenter/scripts/socat_start.sh stop
+/jffs/softcenter/scripts/socat_config.sh stop
 echo_date 只删除web界面和启动脚本，二进制仍在。
 
 find /jffs/softcenter/init.d/ -name "*socat*"|xargs rm -rf
 rm -f /jffs/softcenter/scripts/socat_config.sh
-rm -f /jffs/softcenter/scripts/socat_start.sh
 rm -f /jffs/softcenter/scripts/socat_status.sh
 rm -f /jffs/softcenter/webs/Module_socat.asp
 rm -f /jffs/softcenter/res/icon-socat.png
-version=${socat_version}
 
+echo_date "移除插件储存的运行参数..."
 values=`dbus list socat_ | cut -d "=" -f 1`
 for value in $values
 do
 dbus remove $value 
 done
-echo_date " socat ${version}已卸载"
+echo_date "移除插件储存的软件中心注册参数..."
+values=$(dbus list softcenter_module_socat | cut -d "=" -f 1)
+for value in $values
+do
+	dbus remove $value
+done
+
+echo_date "socat 端口转发 已卸载"
 rm -f /jffs/softcenter/scripts/uninstall_socat.sh
