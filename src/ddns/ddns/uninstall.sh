93385f18e36ca83418b1644b78c698aa6e727575
superzjg <115226261+superzjg@users.noreply.github.com>
2025-07-26 20:38:11 +0800

    Add files via upload

--- src/ddns/ddns/uninstall.sh
+++ src/ddns/ddns/uninstall.sh
@@ -6,4 +6,10 @@ rm -rf /jffs/softcenter/res/icon-ddns.png
 rm -rf /jffs/softcenter/scripts/ddns*
 rm -rf /jffs/softcenter/webs/Module_ddns.asp
 rm -rf /jffs/softcenter/init.d/*ddns.sh
+rm -rf /jffs/softcenter/bin/ddns-go
+if [ -f "/jffs/softcenter/bin/.ddns_go_config.yaml" ];then
+	mkdir -p /tmp/bak
+	echo 【$(TZ=UTC-8 date -R +%Y年%m月%d日\ %X)】: 发现隐藏的配置文件/jffs/softcenter/bin/.ddns_go_config.yaml，移动至临时目录/tmp/bak，重启路由器丢失，有需要请尽快备份！
+	mv -f /jffs/softcenter/bin/.ddns_go_config.yaml /tmp/bak/
+fi
 rm -rf /jffs/softcenter/scripts/uninstall_ddns.sh
